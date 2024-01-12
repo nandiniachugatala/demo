b41906585813a61dc32b40af372351e292e1dd18
nandiniAchugatla <155294588+nandiniAchugatla@users.noreply.github.com>
2024-01-12 19:51:50 +0530

    git commit -m "add"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { C1Component } from './components/c1/c1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,C1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
